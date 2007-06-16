8296940f4bfbc64c77c6443167a563186c133322
Bernhard Walle <bernhard@bwalle.de>
2007-06-16 10:33:48 +0000

    - size fixes by Rodrigo Castro <rodrigo.castro@mezzano.com.br>
    (Old svn revision: 2838)

--- panel-plugin/netload.c
+++ panel-plugin/netload.c
@@ -311,7 +311,7 @@ static void monitor_set_orientation (XfcePanelPlugin *plugin, GtkOrientation ori
                        GTK_WIDGET(global->monitor->label),
                        FALSE, FALSE, 0);
 
-    gtk_container_set_border_width(GTK_CONTAINER(global->monitor->box), 4);
+    gtk_container_set_border_width(GTK_CONTAINER(global->monitor->box), 2);
     gtk_widget_show(GTK_WIDGET(global->monitor->box));
 
     for (i = 0; i < SUM; i++)
