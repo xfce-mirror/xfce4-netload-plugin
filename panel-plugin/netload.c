d35e3a59a6369845a835270b2e4f380950e04b03
Landry Breuil <landry@xfce.org>
2016-06-19 14:58:43 +0200

    set progressbar minimal width/height to 4px so that it looks like with gtk+2

--- panel-plugin/netload.c
+++ panel-plugin/netload.c
@@ -549,7 +549,9 @@ static void set_progressbar_csscolor(GtkWidget* pbar, GdkRGBA* color)
     GtkCssProvider *css_provider;
     gchar * css;
 #if GTK_CHECK_VERSION (3, 20, 0)
-    css = g_strdup_printf("progressbar progress { background-color: %s; background-image: none; }",
+    css = g_strdup_printf("progressbar trough { min-width: 4px; min-height: 4px; } \
+                           progressbar progress { min-width: 4px; min-height: 4px; \
+                                                  background-color: %s; background-image: none; }",
 #else
     css = g_strdup_printf(".progressbar { background-color: %s; background-image: none; }",
 #endif
