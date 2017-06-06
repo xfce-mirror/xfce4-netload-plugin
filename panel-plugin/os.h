3c9496434768e9a23c88bc1bf2b4eaaf689c7cef
Adam Borowski <kilobyte@angband.pl>
2017-06-06 23:25:29 +0200

    Remove sys/sysctl.h on linux platform
    This include is useless and can be removed.
    This syscall has been strongly deprecated for ages and the plugin doesn't use it any more.  On old architectures, this include is benign as long as it's unused, on new ones (such as x32) it results in a build failure.
    Fix bug #11477

--- panel-plugin/os.h
+++ panel-plugin/os.h
@@ -170,7 +170,6 @@
 #elif __linux__             /* L I N U X */
 #  include <stdio.h>
 #  include <sys/param.h>
-#  include <sys/sysctl.h>
 #  include <stdlib.h>
 #  include <stdarg.h>
 #  include <unistd.h>
