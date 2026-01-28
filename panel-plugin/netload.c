xfce-mirror/xfce4-netload-plugin

commit 560900b60c6d08730f8aded26cbc3a6bc70da37c

Allow for admins to deploy a system-wide rc file

panel-plugin/netload.c
@@ -754,7 +754,7 @@ static void monitor_read_config(XfcePanelPlugin *plugin, t_global_monitor *globa
     char *file;
     XfceRc *rc;
     
-    if (!(file = xfce_panel_plugin_save_location (plugin, TRUE)))
+    if (!(file = xfce_panel_plugin_lookup_rc_file (plugin)))
         return;
     
     rc = xfce_rc_simple_open (file, FALSE);

commit 0e74c7b397e9149ab2beacdefa404b9c5f88eef6

Create config file if it doesn't exist
Fixes: 560900b60c6d08730f8aded26cbc3a6bc70da37c

panel-plugin/netload.c
@@ -754,7 +754,8 @@ static void monitor_read_config(XfcePanelPlugin *plugin, t_global_monitor *globa
     char *file;
     XfceRc *rc;
     
-    if (!(file = xfce_panel_plugin_lookup_rc_file (plugin)))
+    if (!(file = xfce_panel_plugin_lookup_rc_file (plugin))
+        && !(file = xfce_panel_plugin_save_location (plugin, TRUE)))
         return;
     
     rc = xfce_rc_simple_open (file, FALSE);